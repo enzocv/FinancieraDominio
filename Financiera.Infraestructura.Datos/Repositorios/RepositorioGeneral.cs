200264200d782b64b49cbbf5d692997479efc694
Usuario <Usuario@AEPIS05>
2017-03-20 16:22:49 -0500

    clase repositorio agregado

---
+++ Financiera.Infraestructura.Datos/Repositorios/RepositorioGeneral.cs
@@ -0,0 +1,43 @@
+﻿using Financiera.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financiera.Infraestructura.Datos.Repositorios
+{
+    public class RepositorioGeneral<T> : IRepositorioGeneral<T>
+    {
+        readonly FinancieraContexto io_contexto;
+        
+        public RepositorioGeneral()
+        {
+            io_contexto = new FinancieraContexto();
+        }
+
+        public T Buscar(params object[] ao_parametros)
+        {
+            return io_contexto.Set<T>().Find(ao_parametros);
+        }
+
+        public void GuardarCambios()
+        {
+            io_contexto.SaveChangesAsync();
+        }
+
+        public void Insertar(T ao_objeto)
+        {
+            io_contexto.Set<T>().Add(ao_objeto);
+        }
+
+        public IList<T> Listar(params object[] ao_parametros)
+        {
+            if (ao_llaves == null)
+                return io_contexto.Set<T>().ToList();
+            else
+                return io_contexto.Set<T>().Where(ao_parametros).ToList();
+        }
+        
+    }
+}
---
+++ Financiera/Repositorios/IRepositorioGeneral.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financiera.Repositorios
+{
+    public interface IRepositorioGeneral<T>
+    {
+        IList<T> Listar(params object[] ao_parametros);
+        T Buscar(params object[] ao_parametros);
+
+        void Insertar(T ao_objeto);
+        void GuardarCambios();
+
+    }
+}
